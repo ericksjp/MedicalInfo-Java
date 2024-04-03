589275ca23429aa6304506d6318e52d44998f473
Erick <ericksjp703@gmail.com>
2024-04-03 19:11:07 -0300

    Adicionando os metodos taxaGordura, litrosAguaDiarios e classificarIMC

--- src/utils/HealthyCalc.java
+++ src/utils/HealthyCalc.java
@@ -10,7 +10,7 @@
 
 package utils;
 
-public abstract class HealthyCalc {
+public class HealthyCalc {
 
   public static float calcularIMC(final float peso, final float altura) {
     if (peso <= 0 || altura <= 0) {
@@ -19,23 +19,42 @@ public abstract class HealthyCalc {
     return peso / (altura * altura);
   }
 
-  public static String classificarIMC() {
-    throw new UnsupportedOperationException("Método não implementado.");
+  public static String classificarIMC(final float imc) {
+    if (imc <= 18.5) {
+      return "Abaixo do Peso";
+    } else if (imc <= 24.9) {
+      return "Peso Normal";
+    } else if (imc <= 29.9) {
+      return "Levemente Acima do Peso";
+    } else if (imc <= 34.9) {
+      return "Obesidade Grau 1";
+    } else if (imc <= 39.9) {
+      return "Obesidade Grau 2";
+    } else {
+      return "Obesidade Grau 3";
+    }
   }
 
-  public static float frequenciaCardiacaAlvo() {
-    throw new UnsupportedOperationException("Método não implementado.");
+  public static float litrosAguaDiarios(final int idade, final float quilos) {
+    if (idade <= 17) {
+      return quilos * 0.040f;
+    } else if (idade < 55) {
+      return quilos * 0.035f;
+    } else if (idade <= 65) {
+      return quilos * 0.030f;
+    } else {
+      return quilos * 0.025f;
+    }
   }
 
-  public static float frequenciaCardiacaMaxima() {
-    throw new UnsupportedOperationException("Método não implementado.");
-  }
-
-  public static float litrosAguaDiarios() {
-    throw new UnsupportedOperationException("Método não implementado.");
-  }
-
-  public static float taxaGordura() {
-    throw new UnsupportedOperationException("Método não implementado.");
+  public static float taxaGordura(final int idade, char sexo, final float imc) {
+    sexo = Character.toUpperCase(sexo);
+    if (sexo == 'M') {
+      return (1.20f * imc) + (0.23f * idade) - (10.8f * 1) - 5.4f;
+    } else if (sexo == 'F') {
+      return (1.20f * imc) + (0.23f * idade) - (10.8f * 0) - 5.4f;
+    } else {
+      throw new IllegalArgumentException("O sexo deve ser 'M' ou 'F'.");
+    }
   }
 }
